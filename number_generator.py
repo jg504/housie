db7020f6a74bb082992e989cbe03033473055402
Jaskaran Gujral <jg504@snu.edu.in>
2020-05-26 21:01:33 +0530

    Create number_generator.py

---
+++ number_generator.py
@@ -0,0 +1,47 @@
+from random import randint
+
+def generator(li):
+    num = randint(1, 90)
+
+    if num in li:
+        num = generator(li)
+    
+    return num
+
+def show(li):
+    print()
+    for i in range(90):
+        if li[i] == 1:
+            print(i + 1, end = " ")
+        
+        if i % 10 == 9:
+            print()
+
+
+li = [0] * 90
+
+count = 0
+
+while count < 90:
+    choice = input("""
+Press 1 for next number.
+Press 2 to show board.
+Press 0 to exit.
+
+>>> """)
+
+    if choice == "0":
+        break
+
+    elif choice == "1":
+        new = generator(li)
+        li[new - 1] = 1
+        print("\n", new)
+    
+    elif choice == "2":
+        show(li)
+    
+    else:
+        print("Wrong choice. Try again.")
+    
+    count += 1
